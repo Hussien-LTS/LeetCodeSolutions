897887ec19f5db940ed28ba418587b76988891d9
Hussein AL Mohamad <60567738+Hussien-LTS@users.noreply.github.com>
2024-01-20 23:22:57 +0300

    Time: 63 ms (15.96%), Space: 49.2 MB (5.12%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let sign = 1;
+    for (let num of nums) {
+        if (num === 0) {
+            return 0;
+        }
+        if (num < 0) {
+            sign = -1 * sign;
+        }
+    }
+    return sign;
+};
